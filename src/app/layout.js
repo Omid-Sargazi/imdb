ef9f62345ce11c1f307d1db80a90b3fd5e21b7d6
OmidSargazi <om.sargazi@gmail.com>
2023-04-30 11:36:52 +0330

    add darkmode for website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,14 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {/* Header  */}
-        <Header />
-        {/* Navbar */}
+      <Providers>
+        <body className={inter.className}>
+          {/* Header  */}
+          <Header />
+          {/* Navbar */}
 
-        {/* SearchBox */}
-        {children}
-      </body>
+          {/* SearchBox */}
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,25 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import {MdLightMode} from 'react-icons/md'
+import {BsFillMoonFill} from 'react-icons/bs'
+import { useTheme } from 'next-themes'
+const DarkMode = () => {
+    const {systemTheme, theme, setTheme} = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(()=>{
+        setMounted(true)
+    },[])
+
+    const currentThem = theme === 'system'? systemTheme : theme;
+  return (
+    <>
+    {mounted&& (currentThem==='dark' ?(<MdLightMode className='text-xl cursor-pointer hover:text-amber-500' onClick={()=>setTheme("light")}/>):( <BsFillMoonFill className='text-xl cursor-pointer hover:text-amber-500' onClick={()=>setTheme('dark')}/>))}
+      
+     
+    </>
+  )
+}
+
+export default DarkMode
+
